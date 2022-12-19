cc7499484f1dfcf5de76fdc55a4a781cd14ef86c
see leng <seelengxd@gmail.com>
2022-12-19 19:53:26 +0800

    feat: add home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,17 @@
+import rightArrow from "../assets/arrow-right.svg";
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div id="home">
+      <h1>
+        This is <span>definitely</span> a real shop.
+      </h1>
+      <Link to="/shop">
+        <button>
+          go to shop <img src={rightArrow} alt="right-arrow" />
+        </button>
+      </Link>
     </div>
   );
 }
